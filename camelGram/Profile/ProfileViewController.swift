625948fa96b19043e7935e105f3966fadec7a767
goonter1998 <9289641@gmail.com>
2023-11-22 18:01:43 +0300

    Добавлены UI и привязаны аутлеты к контроллеру

---
+++ camelGram/Profile/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  camelGram
+//
+//  Created by user on 20.11.2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var loginNameLabel: UILabel!
+    @IBOutlet weak var discriptionNameLabel: UILabel!
+    @IBOutlet weak var logoutButton: UIButton!
+    
+}
