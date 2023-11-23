c9c21cbb464c05fb97e173e10a49c0cb0bfc6b0f
goonter1998 <9289641@gmail.com>
2023-11-23 03:08:40 +0300

    Константы добавлены

---
+++ Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  camelGram
+//
+//  Created by user on 23.11.2023.
+//
+
+import Foundation
+
+let AccessKey = "_FmEN7c8_H4Zsfl7zYWp0K2iDJd9gD1sj4JjQN7VtFM"
+let SecretKey = "XVMpHJVrjQi4F5oqrlsvvO_qJhcONaJLuJILzB6csUU"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com/")!
